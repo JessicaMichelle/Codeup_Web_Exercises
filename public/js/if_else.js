00a075f6eea3287991b8863e7d7fea1acd54b5ef
Jessica Michelle <JessicaMichelle@Jessicas-MacBook-Air.local>
2015-04-25 10:07:47 -0500

    Updated the var message and all strings under each if/else statment.

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -2,26 +2,27 @@ var colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
 var color = colors[Math.floor(Math.random()*colors.length)];
 
 var favorite = 'Green';
+var message;
 
 console.log(color);
 
 if (color=="Green") {
-	var message =("Green is the color of grass.");
+	message ="Green is the color of grass.";
 }
 else if (color=="Red") {
-	var message =("Red is the color of blood.");
+	message ="Red is the color of blood.";
 }
 else if (color=="Orange") {
-	var message =("Orange is the color of an orange.");
+	message ="Orange is the color of an orange.";
 }
 else if (color=="Yellow") {
-	var message =("Yellow is the color of a banana.");
+	message ="Yellow is the color of a banana.";
 }
 else if (color=="Blue") {
-	var message =("Blue is the color of the sky.");
+	message ="Blue is the color of the sky.";
 }
 else {
-	var message =("I do not know anything by that color.");
+	message ="I do not know anything by that color.";
 }
 
 var favMessage = (favorite==color) ? "Your favorite color matches." : "Your favorite color does not match.";

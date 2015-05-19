cf20a360c6425747e832243384dd3fde4e6016d5
Jessica Michelle <JessicaMichelle@Jessicas-MacBook-Air.local>
2015-05-18 20:29:22 -0500

    Updated fadeToggle to slideToggle.

--- public/js/jquery_faq.js
+++ public/js/jquery_faq.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
 	// Reveals and hides the definition in the list when definition term is clicked
 	$("ol>li").click(function(event) {
-		$(this).next().fadeToggle(1000);
+		$(this).next().slideToggle(300);
 	});
 	// Bolds the first line of each unordered list
 	$("ul").each(function(index) {
@@ -11,6 +11,6 @@ $(document).ready(function() {
 	});
 	// Reveals and hides the facts about each National Park when the park name is clicked
 	$("h3").click(function(event) {
-		$(this).next().fadeToggle(1000);
+		$(this).next().slideToggle(200);
 	});
 });

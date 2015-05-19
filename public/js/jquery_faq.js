fe4545c301165c4ff8f73b38d86e64f9e01b7499
Jessica Michelle <JessicaMichelle@Jessicas-MacBook-Air.local>
2015-05-18 19:39:16 -0500

    Added National Parks and facts under the FAQ's - Added toggleclass to reveal facts about parks when park name is clicked - adjusted the toggle selectors to descendants since both OL's and UL's contain LI's.

--- public/js/jquery_faq.js
+++ public/js/jquery_faq.js
@@ -1,7 +1,17 @@
 "use strict";
 
 $(document).ready(function() {
-	$("li").click(function(event) {
+	// Reveals and hides the definition in the list when definition term is clicked
+	$("ol>li").click(function(event) {
+		event.preventDefault();
+		$(this).next().toggleClass("invisible");
+	});
+	// Bolds the first line of each unordered list
+	$("ul").each(function(index) {
+		$(this).children().first().css("font-weight","bold");
+	});
+	// Reveals and hides the facts about each National Park when the park name is clicked
+	$("h3").click(function(event) {
 		event.preventDefault();
 		$(this).next().toggleClass("invisible");
 	});

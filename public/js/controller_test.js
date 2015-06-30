6a6b832e019bcc2a78a3df8b58a5b43fd0168210
Jessica Michelle <JessicaMichelle@Jessicas-MacBook-Air.local>
2015-06-29 20:19:42 -0500

    Adjusted the layout. Spacing was not correct.

--- public/js/controller_test.js
+++ public/js/controller_test.js
@@ -1,21 +1,21 @@
 "use strict";
 
 (function() {
-		var app = angular.module("controllerTest", []);
+	var app = angular.module("controllerTest", []);
 
-		 app.controller("CartController", function() {
-		 	this.items = [];
-		 	this.newItem = {
-		 		quantity: 1
-		 	};
-		 	console.log(this.items)
-		 	this.addItem = function() {
-		 		this.items.push(this.newItem);
-		 		this.newItem = {
-		 			quantity: 1
-		 		};
-	 			console.log(this.items)
-		 	}
+	 app.controller("CartController", function() {
+	 	this.items = [];
+	 	this.newItem = {
+	 		quantity: 1
+	 	};
+	 	console.log(this.items)
+	 	this.addItem = function() {
+	 		this.items.push(this.newItem);
+	 		this.newItem = {
+	 			quantity: 1
+	 		};
+ 			console.log(this.items)
+	 	}
 
-    });
+	});
 })()
